1ae82b2ea4287ac386b8b9e9aab5b98fbeb183ed
Deb-bie <asamoahjaeldeborah@gmail.com>
2024-07-23 20:34:56 +0000

    auth-service: added a method to validate an email

--- auth-service/src/main/java/com/handihub/auth_service/services/AuthService.java
+++ auth-service/src/main/java/com/handihub/auth_service/services/AuthService.java
@@ -4,6 +4,7 @@ import com.handihub.auth_service.dtos.ResponseHandler;
 import com.handihub.auth_service.dtos.SignUpRequest;
 import com.handihub.auth_service.entities.User;
 import com.handihub.auth_service.exceptions.AlreadyExistsException;
+import com.handihub.auth_service.exceptions.EmailNotValid;
 import com.handihub.auth_service.exceptions.NotNull;
 import com.handihub.auth_service.repository.AuthRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +13,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+//    TODO: Test the service
+//    TODO: check if the email already exists
+//    TODO: check to see if an email has an @ and a dot at the right places, if not then that's not an email
+//    TODO: check the password length, it should be more than 6 characters and throw the appropriate exceptions where needed
+
+
     @Autowired
     private AuthRepository authRepository;
 
-    public ResponseHandler signUpUsingEmail(SignUpRequest signUpRequest) throws AlreadyExistsException, NotNull {
+    public ResponseHandler signUpUsingEmail(SignUpRequest signUpRequest) throws AlreadyExistsException, NotNull, EmailNotValid {
         if(authRepository.existsByEmail(signUpRequest.getEmail())) {
             throw new AlreadyExistsException("This email address is already in use");
         } else {
             if (signUpRequest.getEmail() != null &&
                     signUpRequest.getUsername() != null &&
                     signUpRequest.getPassword() != null) {
-                User newUser = User.builder()
-                        .email(signUpRequest.getEmail())
-                        .username(signUpRequest.getUsername())
-                        .password(signUpRequest.getPassword())
-                        .build();
-                authRepository.save(newUser);
-
+                if (validateEmail(signUpRequest.getEmail())){
+                    User newUser = User.builder()
+                            .email(signUpRequest.getEmail())
+                            .username(signUpRequest.getUsername())
+                            .password(signUpRequest.getPassword())
+                            .build();
+                    authRepository.save(newUser);
+                } else {
+                    throw new EmailNotValid("Email is not valid!");
+                }
             } else {
                 throw new NotNull("The email, username and password cannot be null");
             }
@@ -38,5 +48,21 @@ public class AuthService {
                 .status(HttpStatus.CREATED)
                 .message("successful")
                 .build();
+    }
+
+
+    public boolean validateEmail(String email){
+        int indexOfAt;
+        int indexOfDot;
+        if(email.contains("@")){
+            indexOfAt=email.indexOf("@");
+            if (email.contains(".")) {
+                indexOfDot=email.indexOf(".");
+                if (indexOfDot > indexOfAt && indexOfDot != (indexOfAt+1)){
+                    return indexOfDot != email.length() - 1 && email.length() - 1 > indexOfDot;
+                }
+            }
+        }
+        return false;
     }
 }
