5c13a69d2ea8fbef2c5f0ba016a16b86b7dcec84
Deb-bie <asamoahjaeldeborah@gmail.com>
2024-07-26 20:04:53 +0000

    auth-service: added response entity to return type

--- auth-service/src/main/java/com/handihub/auth_service/controllers/AuthController.java
+++ auth-service/src/main/java/com/handihub/auth_service/controllers/AuthController.java
@@ -3,10 +3,11 @@ package com.handihub.auth_service.controllers;
 import com.handihub.auth_service.dtos.ResponseHandler;
 import com.handihub.auth_service.dtos.SignUpRequest;
 import com.handihub.auth_service.exceptions.AlreadyExistsException;
-import com.handihub.auth_service.exceptions.EmailNotValid;
+import com.handihub.auth_service.exceptions.NotValid;
 import com.handihub.auth_service.exceptions.NotNull;
 import com.handihub.auth_service.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseHandler signUpUsingEmail(@RequestBody SignUpRequest signUpRequest) throws AlreadyExistsException, NotNull, EmailNotValid {
+    public ResponseEntity<ResponseHandler> signUpUsingEmail(@RequestBody SignUpRequest signUpRequest) throws AlreadyExistsException, NotNull, NotValid {
         return authService.signUpUsingEmail(signUpRequest);
     }
 
