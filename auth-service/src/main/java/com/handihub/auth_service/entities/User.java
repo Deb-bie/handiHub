203817dbf9057f4d8c4e6e583ffad7e6c9d00ec2
Deb-bie <asamoahjaeldeborah@gmail.com>
2024-08-05 15:43:33 +0000

    auth-service: added methods

--- auth-service/src/main/java/com/handihub/auth_service/entities/User.java
+++ auth-service/src/main/java/com/handihub/auth_service/entities/User.java
@@ -6,16 +6,20 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "regular_user")
+@Table(name = "users")
 @AllArgsConstructor
 @Data
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
@@ -33,4 +37,34 @@ public class User {
     private LocalDateTime createdDate;
 
     private LocalDateTime lastModified;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
